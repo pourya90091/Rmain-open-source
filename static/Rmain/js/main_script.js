49fe2bb0b753dc203fd07489aab41469ff6121fa
pourya90091 <pourya90091@gmail.com>
2022-08-08 20:19:06 +0430

    support up & down arrow keys and improve Enter supporting

--- static/Rmain/js/main_script.js
+++ static/Rmain/js/main_script.js
@@ -1,8 +1,27 @@
 const inputs = document.querySelector("div.inputs");
+inputs.firstElementChild.focus();
+
 if (inputs) {
-    inputs.addEventListener("keypress", function (key_data) {
+    inputs.addEventListener("keydown", function (key_data) {
+        let focus = document.getSelection();
+        let allInputs = inputs.querySelectorAll(".input:not([type='file'])");
+        let index = Math.floor(focus.focusOffset / 2);
+        
         if (key_data.key === "Enter") {
-            document.querySelector("button.btn").click();
+            if (focus.focusNode === inputs) {
+                if (index + 1 < allInputs.length) {
+                    allInputs[index + 1].focus();
+                }
+                else {
+                    document.querySelector("button.btn").click();
+                }
+            }
+        }
+        else if (key_data.key === "ArrowDown") {
+            try {allInputs[index + 1].focus();} catch {}
+        }
+        else if (key_data.key === "ArrowUp") {
+            try {allInputs[index - 1].focus();} catch {}
         }
     });
 }
